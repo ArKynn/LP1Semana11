ff294b6ff2838de29be1c19711e303a39cb40d24
David Mendes <david.f.mendes@proton.me>
2023-05-24 17:05:38 +0100

    Added Starting Players

--- PlayerManagerMVC/Program.cs
+++ PlayerManagerMVC/Program.cs
@@ -4,7 +4,10 @@ public class Program
 {
     public static void Main()
     {
-        List<Player> players = new List<Player>();
+        List<Player> players = new List<Player>() {
+            new Player("Best player ever", 100),
+            new Player("An even better player", 500)
+        };
 
         Controller controller = new Controller(players);
 
