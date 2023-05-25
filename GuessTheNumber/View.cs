8f6df702f2f91f8da08b4780df3c73f1b4658605
David Mendes <david.f.mendes@proton.me>
2023-05-25 08:59:50 +0100

    Added View ShowMessage Method.

--- GuessTheNumber/View.cs
+++ GuessTheNumber/View.cs
@@ -8,5 +8,30 @@ namespace GuessTheNumber
         {
             
         }
+        
+        public static void ShowMessage(string name)
+        {
+            switch (name)
+            {
+                case "GreetUser":
+                    Console.WriteLine("Welcome to Guess the Number!");
+                    Console.WriteLine("I have chosen a number between 1 and 100.");
+                    break;
+                
+                case "LowGuess":
+                    Console.WriteLine("Too low! Try again.");
+                    break;
+                
+                case "HighGuess":
+                    Console.WriteLine("Too high! Try again.");
+                    break;
+                
+                case "CorrectGuess":
+                    Console.WriteLine(
+                        "Congratulations! You guessed the number correctly!");
+                    Console.WriteLine("Number of attempts: " + Controller.Attempts);
+                    break;
+            }
+        }
     }
 }
